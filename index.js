3540fe800e26bda162716e5c925d15bb414415cf
adhfarg <153331802+adhfarg@users.noreply.github.com>
2023-12-23 15:42:50 -0500

    ”completed”

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(ArrayofNames){
+    let messagesarray=[]
+    for (let i =0; i < ArrayofNames.length; i++){
+        messagesarray .push(`Thank you, ${ArrayofNames[i]}, for the wonderful surprise gift!`);
+
+    }
+    return messagesarray 
+
+}
+function countDown(number){
+    while (number >= 0) {
+        console.log(number--);
+      }
+}
